44cb8d2bbbf575bdfa828dd3d314414618c4c29c
glckfndr <obulan65@gmail.com>
2023-10-15 12:16:59 +0300

    feat: Implement caesar_cipher method

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,3 +1,17 @@
 def caesar_cipher(str, number)
+  coded_str = ''
+  str.chars.each do |ch|
+    code = ch.ord
 
+    [('A'..'Z'), ('a'..'z')].each do |range|
+      next unless range.include? ch
+
+      code += number
+      code = range.first.ord + (code - range.last.ord - 1) if code > range.last.ord
+      break
+    end
+
+    coded_str += code.chr
+  end
+  coded_str
 end
